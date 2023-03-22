9c28e62f3866590a114bc10ac7aa34525e067bb6
amarolencyv <amaroverylen06@gmail.com>
2023-03-22 17:59:49 -0400

    changes

---
+++ Azure Scripts/SignInScript.ps1
@@ -0,0 +1,16 @@
+function InstallModules{
+    param([String[]]$names)
+    $DebugPreference = 'Continue'
+    $getAzModules = (Get-Module).Name
+    foreach ($module in $names){
+        if ($module -notin $getAzModules){
+            Write-Host "Installing and importing $module module" -ForegroundColor Yellow
+            Install-Module $module -AllowClobber -Repository PSGallery -Force
+            Import-Module $module -ErrorAction SilentlyContinue
+        }
+        else{
+            Write-Host "The $module module is already installed" -ForegroundColor Green
+        }
+    }
+}
+InstallModules -names "Az.Accounts", "Az.Resources", "Az.LogicApp"
