ca4ed38ccbeed846984c6771370ec20e03b647d2
amarolencyv <amaroverylen06@gmail.com>
2023-03-29 17:50:42 -0400

    testing

--- azure_scripts/SignInScript.ps1
+++ azure_scripts/SignInScript.ps1
@@ -20,3 +20,17 @@ InstallModules -names "Az"
 
 New-AzResourceGroup -Name "testing-resource-group-123" -Tag @{Empty=$null;Department="change"} -Location "eastus"
 #creating resource group to hold other resources in a single location
+
+Get-AzStorageAccount -ResourceGroupName "testRG2" -Name "testrgtacc2"
+$account_name = "testrg2acc2"
+$account_key = "Kknhm690Wcrx+qJi3V8ENyl9BTxo4TVYqixPK3rgjvwMqcUAE3RUJA7a009ESaNQc92W5BZzpy7x+AStPBYKIA=="
+
+$context = New-AzStorageContext -StorageAccountName $account_name -StorageAccountKey $account_key 
+
+try{
+    Get-AzStorageBlob -Container testing -Blob "yourmom.txt" -Context $context
+} catch {
+    New-AzStorageContainer -Name "testing" -Context $context
+    Set-AzStorageBlobContent -Blob "yourmom.txt" -Containter "testing" -File ".\azure_scripts\yourmom.txt"
+    
+}
