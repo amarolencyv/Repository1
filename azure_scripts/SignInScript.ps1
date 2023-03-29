1916d38143ab064c4b81f4f435d3a9be57df6a8b
amarolencyv <amaroverylen06@gmail.com>
2023-03-29 18:24:59 -0400

    whatever

--- azure_scripts/SignInScript.ps1
+++ azure_scripts/SignInScript.ps1
@@ -26,7 +26,7 @@ $account_name = "testrg2acc2"
 $account_key = "Kknhm690Wcrx+qJi3V8ENyl9BTxo4TVYqixPK3rgjvwMqcUAE3RUJA7a009ESaNQc92W5BZzpy7x+AStPBYKIA=="
 
 $context = New-AzStorageContext -StorageAccountName $account_name -StorageAccountKey $account_key 
-
+#lkjh
 try{
     Get-AzStorageBlob -Container "testing" -Blob "yourmom.txt" -Context $context
 } catch {
